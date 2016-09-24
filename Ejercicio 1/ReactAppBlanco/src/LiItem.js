bcae47ecc623b9122bfb1b4883366a47c01fb0e3
SebasRiosR <sebastian.riosr@udea.edu.co>
2016-09-24 11:55:57 -0500

    Los dos ejercicios

---
+++ Ejercicio 1/ReactAppBlanco/src/LiItem.js
@@ -0,0 +1,12 @@
+import React,{Componen} from 'react';
+var LiItem = React.createClass ({
+	render: function () {
+		return (
+			<div>
+				<button onClick={this.props.borrar.bind(this.props.itemText,this)} >x</button>
+				<label>{this.props.itemText}</label>
+			</div>
+		)
+	}
+});
+export default LiItem;
---
+++ Ejercicio 2/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MotelActions from './actions/MotelActions';
+import MotelGrid from './components/Motelgrid.jsx';
+
+setInterval(function () {
+	MotelActions.fetchlist();
+	}, 5000
+);
+ReactDOM.render(<MotelGrid/>, document.getElementById('app'));
